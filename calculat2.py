f59188370d50c966351b7771aad577d33b14fa92
ADEDOLAPO AYODEJI <ayodejiadedolapo@gmail.com>
2024-12-20 13:16:26 -0700

    calculator

--- calculat2.py
+++ calculat2.py
@@ -3,6 +3,12 @@ def expo(num1, num2):
     return num1 ** num2
 
 
+
+def add(num1, num2):
+    return num1 + num2
+
+
+
 def floor(num1, num2):
     if num2 == 0:
         return "Error can't  be divisible by zero"
@@ -14,7 +20,7 @@ def modulus(num1, num2):
     return num1 % num2
 
 
-choice = input("Enter between 1- 3: ")
+choice = input("Enter between 1- 4: ")
 
 num1 = int(input("Enter first number:  "))
 num2 = int(input("Enter second number: "))
@@ -25,6 +31,8 @@ elif choice == "2":
     print(f"the result of floor- division:{floor(num1, num2)}")
 elif choice == "3":
     print(f"the result of modulus: {modulus(num1,num2)}")
+elif choice == "4":
+    print(f"the result of add: {add(num1,num2)}")
 else:
     print("invalid choice of calculator")
 
